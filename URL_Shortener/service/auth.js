125bd4caf402c4323c2167961fc2cc4ff6226797
Aniket Shelake <aniketshelake151@sitcoe.org.in>
2024-03-22 21:37:06 +0530

    modified file

--- URL_Shortener/service/auth.js
+++ URL_Shortener/service/auth.js
@@ -1,14 +1,27 @@
-const sessionIdToUserMap = new Map(); //? hashmap
+const jwt = require("jsonwebtoken");
+const secret = "aniket@123@$"; // this is secret key
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  ); // this is like a stamp
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null; // token not found
+   
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null; // invalid token
+  }
 }
 
 module.exports = {
-    setUser,
-    getUser
-}
+  setUser,
+  getUser,
+};
